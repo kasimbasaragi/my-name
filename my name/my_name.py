a8019b707b53bfec3d17c2c1bbb5ed896b8654f7
kasimbasaragi <kasimbasaragi@gmail.com>
2025-05-24 16:39:14 +0530

    Add Flask app my_name.py

---
+++ my name/my_name.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    full_name = None
+    if request.method == "POST":
+        first = request.form.get("first_name")
+        last = request.form.get("last_name")
+        full_name = f"{first} {last}"
+    return render_template("form.html", full_name=full_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
